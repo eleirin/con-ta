1805f677b875c69c302290f109888c8e134fda69
^,^ <joy.void.joy@gmail.com>
2018-06-18 05:38:14 +0200

    First steps toward a proper MainWindow
    Add a central layout to MainWindow.
    Add two lists, each corresponding to a specific model, and display them

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,17 +1,34 @@
 #include "MainWindow.h"
 #include <QStringListModel>
+#include <QFileSystemModel>
 #include <QStringList>
 #include <QListView>
+#include <QGridLayout>
 
-MainWindow::MainWindow(void)
+MainWindow::MainWindow(void):
+    m_ListEntity(new QStringListModel()),
+    m_ListFile(new QFileSystemModel())
 {
-    QStringListModel *model = new QStringListModel();
-    QStringList list = {"a", "b", "c"};
-    model->setStringList(list);
+    /*** Main Layout ***/
+    QWidget *window = new QWidget;
+        QGridLayout *mainLayout = new QGridLayout;
+        {
+            QListView *entityList = new QListView;
+            entityList->setModel(m_ListEntity);
+            entityList->setEditTriggers(QAbstractItemView::NoEditTriggers);
+            entityList->setViewMode(QListView::ViewMode::IconMode);
+            mainLayout->addWidget(entityList);
 
-    QListView *central = new QListView;
-    central->setModel(model);
-    central->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    central->setViewMode(QListView::ViewMode::IconMode);
-    setCentralWidget(central);
+            QListView *fileList = new QListView;
+            fileList->setModel(m_ListFile);
+            fileList->setViewMode(QListView::ViewMode::IconMode);
+            fileList->setRootIndex(m_ListFile->index(QDir::currentPath()));
+            mainLayout->addWidget(fileList);
+        }
+        window->setLayout(mainLayout);
+    setCentralWidget(window);
+
+    /*** Member initialisation ***/
+    m_ListEntity->setStringList({"a", "b"});
+    m_ListFile->setRootPath("~/oeuvre/finance/");
 }
--- MainWindow.h
+++ MainWindow.h
@@ -1,10 +1,16 @@
 #ifndef HEAD_MAINWINDOW
 #define HEAD_MAINWINDOW
 #include <QMainWindow>
+class QStringListModel;
+class QFileSystemModel;
 
 class MainWindow: public QMainWindow
 {
 public:
     MainWindow(void);
+
+private:
+    QStringListModel *m_ListEntity;
+    QFileSystemModel *m_ListFile;
 };
 #endif //HEAD_MAINWINDOW
