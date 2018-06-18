de02a9ea88bdb343b738d1103e666f37a9bee490
^,^ <joy.void.joy@gmail.com>
2018-06-19 00:56:49 +0200

    feat: Have a NewBill pop up when clicking a file

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,16 +1,22 @@
 #include "MainWindow.h"
+#include "NewBill.h"
 #include <QStringListModel>
 #include <QFileSystemModel>
 #include <QStringList>
+#include <QModelIndex>
 #include <QListView>
 #include <QGridLayout>
 #include <QGroupBox>
 #include <QLabel>
 
+#include <iostream>
+
 MainWindow::MainWindow(void):
     m_ListEntity(new QStringListModel()),
     m_ListFile(new QFileSystemModel())
 {
+    QListView *fileList;
+
     /*** Main Layout ***/
     QWidget *window = new QWidget;
         QGridLayout *mainLayout = new QGridLayout;
@@ -21,9 +27,10 @@ MainWindow::MainWindow(void):
             entityList->setViewMode(QListView::ViewMode::IconMode);
             mainLayout->addWidget(entityList, 0, 0);
 
-            QListView *fileList = new QListView;
+            fileList = new QListView;
             fileList->setModel(m_ListFile);
             fileList->setRootIndex(m_ListFile->index("./assets"));
+
             mainLayout->addWidget(fileList, 0, 1, 2, 1);
 
             QGroupBox *infogroupbox = new QGroupBox(tr("Informations"));
@@ -43,7 +50,16 @@ MainWindow::MainWindow(void):
         window->setLayout(mainLayout);
     setCentralWidget(window);
 
+    /*** Action behaviour and slot connection ***/
+    connect(fileList, &QListView::clicked, this, &MainWindow::openNewBill);
+
     /*** Member initialisation ***/
     m_ListEntity->setStringList({"a", "b"});
     m_ListFile->setRootPath("~/oeuvre/finance/");
+}
+
+void MainWindow::openNewBill(const QModelIndex &index)
+{
+    QString item = m_ListFile->filePath(index);
+    NewBill(item).exec();
 }
--- MainWindow.h
+++ MainWindow.h
@@ -3,11 +3,15 @@
 #include <QMainWindow>
 class QStringListModel;
 class QFileSystemModel;
+class QModelIndex;
 
 class MainWindow: public QMainWindow
 {
 public:
     MainWindow(void);
+
+private slots:
+    void openNewBill(const QModelIndex &index);
 
 private:
     QStringListModel *m_ListEntity;
