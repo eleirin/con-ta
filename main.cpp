ab6019246ca5c5a426c01acb3413da07347a876d
^,^ <joy.void.joy@gmail.com>
2018-06-18 17:50:12 +0200

    fix: Rectify database name

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     /*** Setting sql connection ***/
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("finance");
+    db.setDatabaseName("finance.db");
     db.open();
 
     /*** Main window display ***/
